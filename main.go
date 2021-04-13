01b4412033af9469d9a2434f9ec15026f3fbf6c2
Johnny Chang <johnnyavy@gmail.com>
2021-04-13 20:04:50 +0800

    new sks package

--- main.go
+++ main.go
@@ -3,9 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
-	"math/rand"
-	"sort"
-	"time"
+
+	"github.com/JohnnyCPC/reservoir-sampling-go/sks"
 )
 
 func main() {
@@ -22,27 +21,7 @@ func main() {
 	ln := len(a)
 	fmt.Println(*nFlag, *k)
 
-	fmt.Println(SelectKItems(a, ln, *k))
-}
-
-func SelectKItems(streams []int, n int, k int) []int {
-
-	var i int
-	reservoir := make([]int, k)
-	rand.Seed(time.Now().UnixNano())
-
-	for i = 0; i < k; i++ {
-		reservoir[i] = streams[i]
-	}
-	for ; i < n; i++ {
-		j := rand.Intn(i + 1)
-		if j < k {
-			reservoir[j] = streams[i]
-		}
-	}
-	sort.Ints(reservoir)
-
-	return reservoir
+	fmt.Println(sks.SelectKItems(a, ln, *k))
 }
 
 func printResult(s []int, n int) {
